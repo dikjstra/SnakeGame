3858498465b10fd29cb554c622fa97a106249621
dijkstra <1010557667@qq.com>
2021-10-28 15:40:56 +0800

    commit

---
+++ src/com/zxz/game/MyPanel.java
@@ -0,0 +1,234 @@
+package com.zxz.game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * @author 周贤筑
+ * @version 1.0
+ * 游戏界面面板
+ */
+public class MyPanel extends JPanel {
+    //该数组存储贪吃蛇x坐标
+    int[] snakeX = new int[200];
+    //该数组存储贪吃蛇y坐标
+    int[] snakeY = new int[200];
+    //食物坐标
+    //int foodX;
+    //int foodY;
+    //食物对象
+    Food food = null;
+    //蛇长
+    int length;
+    //蛇存活状态
+    boolean isLive = true;
+    //蛇朝向，默认向右
+    String direction;
+    //游戏默认暂停
+    boolean isStart = false;
+    //设置游戏所需图片
+    Image bodyImg = null;
+    Image bodyDownImg = null;
+    Image bodyUpImg = null;
+    Image bodyLeftImg = null;
+    Image bodyRightImg = null;
+    Image foodImg = null;
+    //设置定时器
+    Timer timer;
+    //设置积分
+    int score;
+
+    public void init(){
+        //初始化方向为R
+        direction = "R";
+        //初始化积分
+        score = 0;
+        //初始化蛇长
+        length = 3;
+        //初始化蛇的头坐标
+        snakeX[0] = 175;
+        snakeY[0] = 275;
+        //身体第一节
+        snakeX[1] = 150;
+        snakeY[1] = 275;
+        //身体第二节
+        snakeX[2] = 125;
+        snakeY[2] = 275;
+        //初始化食物坐标
+        food = new Food(250,300);
+
+        //初始化图片
+        //image1 = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/bomb_1.gif"));
+        bodyImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/body.png"));
+        bodyDownImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/down.png"));
+        bodyUpImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/up.png"));
+        bodyLeftImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/left.png"));
+        bodyRightImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/right.png"));
+        foodImg = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/image/food.png"));
+
+        timer = new Timer(100, new ActionListener() {
+            //蛇移动
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(isStart && isLive){
+                    //后一节身体替换前一节身体
+                    for (int i = length - 1; i > 0; i--) {
+                        snakeX[i] = snakeX[i - 1];
+                        snakeY[i] = snakeY[i - 1];
+                    }
+                    if(direction == "R"){
+                        snakeX[0] += 25;
+                        if(snakeX[0] > 800){
+                            snakeX[0] = 0;
+                        }
+                    }else if(direction == "L"){
+                        snakeX[0] -= 25;
+                        if(snakeX[0] < 0){
+                            snakeX[0] = 775;
+                        }
+                    }
+                    else if (direction == "U"){
+                        snakeY[0] -= 25;
+                        if(snakeY[0] < 50){
+                            snakeY[0] = 775;
+                        }
+                    }
+                    else if (direction == "D"){
+                        snakeY[0] += 25;
+                        if(snakeY[0] > 800){
+                            snakeY[0] = 75;
+                        }
+                    }
+                    //蛇吃到食物
+                    if(snakeX[0] == food.getX() && snakeY[0] == food.getY()){
+                        length++;
+                        score += 10;
+                        food.setX((int)((Math.random() * 31) + 1) * 25);//25 - 800
+                        food.setY((int)((Math.random() * 28) + 3) * 25 );//75 - 800
+                    }
+                    //死亡条件
+                    for (int i = 1; i < length; i++) {
+                        if(snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]){
+                            isLive = false;
+                        }
+                    }
+                    repaint();
+                }
+            }
+        });
+        //启动定时器
+        timer.start();
+    }
+
+    public MyPanel(){
+        init();
+        //将焦点定位在面板上
+        this.setFocusable(true);
+        //加入监听
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                if(keyCode == KeyEvent.VK_SPACE){
+                    if(isLive){
+                        isStart = !isStart;
+                        repaint();
+                    }else{
+                        timer.stop();
+                        init();//初始化游戏
+                        isLive = true;
+                    }
+                }
+                if(isStart){
+                    switch(e.getKeyCode()){
+                        case KeyEvent.VK_W:
+                            if(direction != "D"){
+                                direction = "U";
+                                repaint();
+                            }
+                            break;
+                        case KeyEvent.VK_A:
+                            if(direction != "R"){
+                                direction = "L";
+                                repaint();
+                            }
+                            break;
+                        case KeyEvent.VK_D:
+                            if(direction != "L"){
+                                direction = "R";
+                                repaint();
+                            }
+                            break;
+                        case KeyEvent.VK_S:
+                            if(direction != "U"){
+                                direction = "D";
+                                repaint();
+                            }
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //填充背景颜色
+        this.setBackground(new Color(236, 223, 175, 62));
+        //游戏区域
+        g.fillRect(0,50,800,750);
+
+        //画蛇头
+        switch(direction){
+            case "R":
+                g.drawImage(bodyRightImg,snakeX[0],snakeY[0],this);
+                break;
+            case "L":
+                g.drawImage(bodyLeftImg,snakeX[0],snakeY[0],this);
+                break;
+            case "U":
+                g.drawImage(bodyUpImg,snakeX[0],snakeY[0],this);
+                break;
+            case "D":
+                g.drawImage(bodyDownImg,snakeX[0],snakeY[0],this);
+                break;
+        }
+        //画蛇身
+        for (int i = 1; i < length; i++) {
+            g.drawImage(bodyImg,snakeX[i],snakeY[i],this);
+        }
+        //画食物
+        g.drawImage(foodImg,food.getX(),food.getY(),this);
+        //画积分
+        g.setColor(new Color(236, 9, 32));
+        g.setFont(new Font("微软雅黑",Font.BOLD,30));
+        g.drawString("当前得分：" + score,25,35);
+        //死亡提示
+       if(!isLive){
+           g.setColor(new Color(222, 210, 2));
+           g.setFont(new Font("微软雅黑",Font.BOLD,30));
+           g.drawString("游戏结束，按下空格后重新开始游戏",250,330);
+       }
+
+        if(!isStart){
+            g.setColor(new Color(222, 210, 2));
+            g.setFont(new Font("微软雅黑",Font.BOLD,40));
+            g.drawString("点击空格开始游戏",250,330);
+        }
+    }
+}
---
+++ src/com/zxz/game/StartGame.java
@@ -0,0 +1,27 @@
+package com.zxz.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author 周贤筑
+ * @version 1.0
+ */
+public class StartGame {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setTitle("贪吃蛇");
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+        //窗体坐标（居中）
+        jf.setBounds((width-800)/2,(height - 800) / 2,800,800);
+        //窗体大小不可调
+        jf.setResizable(false);
+        //关闭窗体即结束程序
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //创建面板
+        MyPanel mp = new MyPanel();
+        jf.add(mp);
+        jf.setVisible(true);
+    }
+}
