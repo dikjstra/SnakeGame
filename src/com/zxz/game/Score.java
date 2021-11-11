d3dd1cc53f9792d7a5dcd2ae169f8057e9ab0f40
Dijkstra <1010557667@qq.com>
2021-11-11 19:52:52 +0800

    commit

---
+++ src/com/zxz/game/Score.java
@@ -0,0 +1,10 @@
+﻿package com.zxz.game;
+
+/**
+ * @author 周贤筑
+ * @version 1.0
+ */
+public class Score {
+    //记录得分
+    public static int scores;
+}
---
+++ src/com/zxz/game/Snake.java
@@ -0,0 +1,58 @@
+package com.zxz.game;
+
+/**
+ * @author 周贤筑
+ * @version 1.0
+ */
+public class Snake {
+    //该数组存储贪吃蛇x坐标
+    private int[] snakeX = new int[200];
+    //该数组存储贪吃蛇y坐标
+    private int[] snakeY = new int[200];
+    //长度
+    private int length;
+    //方向
+    private String direction;
+
+    public Snake() {
+    }
+
+    public Snake(int[] snakeX, int[] snakeY, int length, String direction) {
+        this.snakeX = snakeX;
+        this.snakeY = snakeY;
+        this.length = length;
+        this.direction = direction;
+    }
+
+    public int[] getSnakeX() {
+        return snakeX;
+    }
+
+    public void setSnakeX(int[] snakeX) {
+        this.snakeX = snakeX;
+    }
+
+    public int[] getSnakeY() {
+        return snakeY;
+    }
+
+    public void setSnakeY(int[] snakeY) {
+        this.snakeY = snakeY;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+}
